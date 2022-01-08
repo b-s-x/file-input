33a1bb0fae20efdbd3c0e5348702a2fa13ba8a67
b-s-x <b.s.x.mars@gmail.com>
2022-01-08 09:45:16 +0300

    @/components/WithSpinner: fix

---
+++ src/components/WithSpinner.vue
@@ -0,0 +1,27 @@
+<script lang="ts" setup>
+import CustomSpinner from './CustomSpinner.vue';
+
+const props = defineProps({
+  isFetching: {
+    type: Boolean,
+    default: false,
+  },
+  inline: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+</script>
+
+<template>
+  <CustomSpinner
+    key="spinner"
+    v-if="props.isFetching"
+    v-bind="$attrs"
+    :inline="props.inline"
+  />
+  <template v-show="!props.isFetching">
+    <slot />
+  </template>
+</template>

8fd230c46e1a09c1fda242a4ebdb4f5f44daa5b2
b-s-x <b.s.x.mars@gmail.com>
2022-02-27 13:46:15 +0300

    @/constants/dates: add dates constant

---
+++ src/constants/dates.ts
@@ -0,0 +1,7 @@
+export const dateFormats = {
+  DEFAULT: 'DD.MM.YYYY',
+  DEFAULT_SLASHED: 'DD/MM/YYYY',
+  LONG: 'DD MMMM YYYY',
+  LONG_TIME: 'DD MMMM YYYY HH:mm',
+  LONG_SHORT_MONTH: 'DD MMM YYYY',
+};
